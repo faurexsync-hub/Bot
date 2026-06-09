c162e72733d19914674ba678f727ae54349b3afe
faurexsync-hub <faurexsync@gmail.com>
2026-06-09 10:25:45 +0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BOT_TOKEN  = os.environ.get("BOT_TOKEN", "BU_YERGA_TOKEN_YOZING")
-ADMIN_ID   = int(os.environ.get("ADMIN_ID", "8355611778"))
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "@fuzzssss")
